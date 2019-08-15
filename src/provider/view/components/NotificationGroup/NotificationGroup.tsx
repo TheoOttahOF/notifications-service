f78482cfd628879d57eb03535fea8ebe99671fa0
Theo Ottah <48205674+TheoOttahOF@users.noreply.github.com>
2019-08-15 17:01:31 +0100

    Fix animation stutter when new notification appears

--- src/provider/view/components/NotificationGroup/NotificationGroup.tsx
+++ src/provider/view/components/NotificationGroup/NotificationGroup.tsx
@@ -38,14 +38,11 @@ export function NotificationGroup(props: Props) {
                                     enter: 300,
                                     exit: 300
                                 }}
+                                in={true}
                                 classNames="item"
                             >
                                 <li key={notification.id}>
-                                    <CSSTransition
-                                        timeout={500}
-                                    >
-                                        <NotificationCard notification={notification} dispatch={dispatch} />
-                                    </CSSTransition>
+                                    <NotificationCard notification={notification} dispatch={dispatch} />
                                 </li>
                             </CSSTransition>
                         );
