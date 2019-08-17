d95622a08aa2c6f63a65d617c4786e69ecaea683
Theo Ottah <48205674+TheoOttahOF@users.noreply.github.com>
2019-08-17 22:56:45 +0100

    Add Markdown support

--- src/provider/view/components/NotificationCard/Body.tsx
+++ src/provider/view/components/NotificationCard/Body.tsx
@@ -1,6 +1,19 @@
 import * as React from 'react';
+import markdown from 'markdown-it';
 
 import './Body.scss';
+
+const md = markdown('zero', {breaks: true});
+
+md.enable('hr');
+md.enable('paragraph');
+md.enable('image');
+md.enable('newline');
+md.enable('heading');
+md.enable('lheading');
+md.enable('list');
+md.enable('blockquote');
+md.enable('emphasis');
 
 interface Props {
     text?: string;
@@ -9,6 +22,6 @@ interface Props {
 export function Body(props: Props) {
     const {text = ''} = props;
     return (
-        <div className="text">{text}</div>
+        <div className="text" dangerouslySetInnerHTML={{__html: md.render(text)}}></div>
     );
 }
